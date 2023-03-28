fdaa538dae1ceb05238ca4ea1fe084ae1ec1afd4
01Gibbs <danielgilbert49@hotmail.co.uk>
2023-03-28 16:14:53 +0100

    geolocation feature branch

--- App.js
+++ App.js
@@ -1,39 +1,74 @@
-import React from 'react'
-import MapView, { Circle } from 'react-native-maps'
-import { Marker } from 'react-native-maps'
-import { StyleSheet, View } from 'react-native'
-import { locations } from './coordinates'
+import React, { useState, useEffect } from "react";
+import MapView, { Circle } from "react-native-maps";
+import { Marker } from "react-native-maps";
+import { StyleSheet, View, Button } from "react-native";
+import { locations } from "./coordinates";
+import * as Location from "expo-location";
 
-export default function App () {
-  function createKey (location) {
-    return location.latitude + location.longitude
+export default function App() {
+  function createKey(location) {
+    return location.latitude + location.longitude;
   }
+
+  const [mapRegion, setMapRegion] = useState({
+    // latitude: null,
+    // longitude: null,
+    // latitudeDelta: null,
+    // longitudeDelta: null,
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+    console.log(location.coords.latitude, location.coords.longitude);
+  };
+
+  useEffect(() => {
+    userLocation();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
-        {locations.map(location => (
+      <MapView style={styles.map} region={mapRegion}>
+        {locations.map((location) => (
           <Marker key={`mk${createKey(location)}`} coordinate={location} />
         ))}
-        {locations.map(location => (
+        {locations.map((location) => (
           <Circle
             key={`c${createKey(location)}`}
             center={location}
             radius={2000}
-            fillColor='rgba(100,100,100,0.2)'
+            fillColor="rgba(100,100,100,0.2)"
           />
         ))}
+        <Marker coordinate={mapRegion} title="Gibbs Edit"></Marker>
       </MapView>
+      <Button title="Get  Location" onPress={userLocation} />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   map: {
-    width: '100%',
-    height: '100%'
-  }
-})
+    width: "100%",
+    height: "100%",
+  },
+});
