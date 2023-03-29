5805da5a43bd0d6c0d536b4d114f5ad2676c9c0c
beckybp <becky.pugh@hotmail.co.uk>
2023-03-29 11:04:33 +0100

    navigation testing

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import React from 'react'
 import MapView, { Circle } from 'react-native-maps'
 import { Marker } from 'react-native-maps'
-import { StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import { locations } from './coordinates'
+import { NavigationContainer } from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App () {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{title: 'Login'}}
+        />
+        <Stack.Screen name="Map" component={Map} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const Login = ({navigation}) => {
+  return (
+    <Button
+      title="Login"
+      onPress={() =>
+        navigation.navigate('Map')
+      }
+    />
+  );
+};
+
+const Map = ({navigation}) => {
+
   function createKey (location) {
     return location.latitude + location.longitude
   }
 
   return (
-    <View style={styles.container}>
+      <View style={styles.container}>
       <MapView style={styles.map}>
         {locations.map(location => (
           <Marker key={`mk${createKey(location)} + ${Math.random()*10}`} coordinate={location} />
@@ -25,8 +57,8 @@ export default function App () {
         ))}
       </MapView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
