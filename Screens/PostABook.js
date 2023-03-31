eb211dc3402c7a43d5d1eba28a1b5e29c16bc014
erinmd <erin817@gmail.com>
2023-03-31 17:02:12 +0100

    form addedd

--- Screens/PostABook.js
+++ Screens/PostABook.js
@@ -1,11 +1,39 @@
-import { Text,  View } from "react-native";
+import { Text,  View, TextInput, Button, StyleSheet } from "react-native";
+import { useState } from 'react'
 
 const PostABook = ({navigation}) => {
+
+  const [postBookForm, setPostBookForm] = useState(null)
+  const [formFields, setFormFields] = useState({title:'', author:'', genre:'', location_description: ''})
+
+  const handlePost = () => {
+    console.log(formFields.title)
+  }
+
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Post your book here!</Text>
+        <TextInput style={styles.input} placeholder='Book Title' value={formFields.title} onChange={(e)=>{setFormFields({...formFields, title:e.target.value})}}></TextInput>
+        <Button title='Submit' onPress={handlePost}/>
       </View>
     );
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    input: {
+      width: '80%',
+      borderWidth: 1,
+      borderColor: 'black',
+      borderRadius: 5,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      marginVertical: 10
+    }
+  })
 
   export default PostABook
