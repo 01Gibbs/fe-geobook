32df7566ba62c8599bd11865ff64985de2f5164a
erinmd <erin817@gmail.com>
2023-03-31 12:31:11 +0100

    single book page added

--- Screens/SingleBook.js
+++ Screens/SingleBook.js
@@ -1,10 +1,28 @@
-import { Button, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
+import { getBook } from "../data/api"
 
 export const SingleBook = ({ route, navigation}) => {
     const {book_id} = route.params
-    return <View>
-        <Text>Book information</Text>
-        <Text>{book_id}</Text>
+    const [bookInfo, setBookInfo] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(()=>{
+        setIsLoading(true)
+        getBook(book_id).then(book => {
+            setBookInfo(book)
+            setIsLoading(false)
+        })
+    },[])
+
+
+    return isLoading? null: <View>
+        <Text>Genre: {bookInfo.genre}</Text>
+        <Text>Location description: {bookInfo.location_description}</Text>
+        <Text>Left by: {bookInfo.posted_by}</Text>
         <Button onPress={()=>navigation.goBack()} title='Go Back'/>
+        <Button title='Found book' />
     </View>
 }
+
+
