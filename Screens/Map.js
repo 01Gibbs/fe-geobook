b63fb4fa72c07a7e172a85d339b7ae955a706c03
01Gibbs <danielgilbert49@hotmail.co.uk>
2023-03-31 10:30:45 +0100

    progress push - MapZoom/userLoc/mapRegion

--- Screens/Map.js
+++ Screens/Map.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button, View } from "react-native";
 import { Circle, Marker } from "react-native-maps";
 import { StyleSheet } from "react-native";
-import MapView from "react-native-map-clustering"
+import MapView from "react-native-map-clustering";
 
 import * as Location from "expo-location";
 import { getBooks } from "../data/api";
@@ -12,6 +12,8 @@ const Map = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [locations, setLocations] = useState(null);
   const [books, setBooks] = useState([]);
+
+  const mapRef = useRef();
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,34 +38,38 @@ const Map = () => {
   }
 
   const [mapRegion, setMapRegion] = useState({
-    // latitude: null,
-    // longitude: null,
-    // latitudeDelta: null,
-    // longitudeDelta: null,
-    latitude: 53.797193, 
-    longitude: -1.556831,
-    latitudeDelta: 10,
-    longitudeDelta: 10,
+    // default UK view (without geolocation)
+    latitude: 53.797193,
+    longitude: -3.656831,
+    latitudeDelta: 15,
+    longitudeDelta: 15,
   });
 
-
-  // const userLocation = async () => {
-  //   // await console.log('Fetching current location')
-  //   let { status } = await Location.requestForegroundPermissionsAsync();
-  //   if (status !== "granted") {
-  //     setErrorMsg("Permission to access location was denied");
-  //   }
-  //   let location = await Location.getCurrentPositionAsync({
-  //     enableHighAccuracy: true,
-  //   });
-  //   // await console.log('Done')
-  //   setMapRegion({
-  //     latitude: location.coords.latitude,
-  //     longitude: location.coords.longitude,
-  //     latitudeDelta: 10,
-  //     longitudeDelta: 10,
-  //   });
-  // };
+  const userLocation = async () => {
+    // await console.log('Fetching current location')
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    // await console.log('Done')
+    const setMapRegionConstant = setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    });
+    const animateToRegion = () => {
+      mapRef.current.animateToRegion(
+        // this.mapView.animateToRegion(
+        setMapRegionConstant,
+        2000
+      );
+    };
+    animateToRegion();
+  };
 
   useEffect(() => {
     userLocation();
@@ -100,7 +106,7 @@ const Map = () => {
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <View style={styles.container}>
-          <MapView style={styles.map} region={mapRegion}>
+          <MapView style={styles.map} region={mapRegion} ref={mapRef}>
             {locations.map((location) => (
               <Marker key={`mk${createKey(location)}`} coordinate={location} />
             ))}
