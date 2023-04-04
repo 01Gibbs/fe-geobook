0864664137222fe7b05be296e4feedec84d408f0
erinmd <erin817@gmail.com>
2023-04-04 16:27:42 +0100

    added styling

--- Screens/FoundBook.js
+++ Screens/FoundBook.js
@@ -1,5 +1,12 @@
 import { useState } from 'react'
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native'
 import Toast from 'react-native-root-toast'
 import { getClaimedBookThumbnail, patchUser } from '../data/api'
 import { auth } from '../firebaseConfig'
@@ -40,32 +47,74 @@ const FoundBook = ({ bookInfo, setFoundBook, navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text>What is the title?</Text>
-      <TextInput
-        value={bookTitle}
-        onChange={e => setBookTitle(e.nativeEvent.text)}
-        style={styles.input}
-      ></TextInput>
-      <Text>{errorMsg}</Text>
-      <Button title='Found book' onPress={handlePress} />
-      <Button title='Go Back' onPress={() => setFoundBook(false)} />
+      <View style={styles.main}>
+        <Text style={styles.title}>What is the title of the book?</Text>
+        <TextInput
+          style={styles.input}
+          value={bookTitle}
+          onChange={e => setBookTitle(e.nativeEvent.text)}
+          style={styles.input}
+        ></TextInput>
+        <Text>{errorMsg}</Text>
+        <Pressable style={styles.submit} onPress={handlePress}>
+          <Text>CLAIM BOOK</Text>
+        </Pressable>
+        <Pressable style={styles.submit} onPress={() => setFoundBook(false)}>
+          <Text>GO BACK</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
     flex: 1,
+    backgroundColor: '#2B5F6B',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignContent: 'center'
+  },
+  header: {
+    alignItems: 'left',
+    alignText: 'left'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 600
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 15
+  },
+  main: {
+    margin: 0,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+    // backgroundColor:'#132235',
+    borderRadius: 5
   },
   input: {
-    width: '80%',
+    alignItems: 'center',
+    alignSelf: 'stretch',
     borderWidth: 1,
     borderColor: 'black',
     borderRadius: 5,
     paddingHorizontal: 10,
     paddingVertical: 5,
-    marginVertical: 10
+    marginTop: 5
+  },
+  submit: {
+    backgroundColor: '#5CDB95',
+    alignSelf: 'stretch',
+    borderRadius: 5,
+    alignItems: 'center',
+    padding: 10,
+    marginBottom: 15
+  },
+  error: {
+    color: 'red'
   }
 })
 export default FoundBook
